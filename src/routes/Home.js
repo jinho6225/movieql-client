9793a414ce287f65f9f129a43b17868e05e9bee0
jinho6225 <jinho6225@hotmail.com>
2020-06-13 19:59:51 -0700

    updated as optional chaining

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -58,17 +58,15 @@ export default () => {
   return (
     <Container>
       <Header>
-        <Title>Apollo 2020</Title>
-        <Subtitle>I love GraphQL</Subtitle>
+        <Title>MovieQL</Title>
+        <Subtitle>I love GraphQL{`&`}Apollo</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading && data.movies && (
-        <Movies>
-          {data.movies.map((m) => (
-            <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
-          ))}
-        </Movies>
-      )}
+      <Movies>
+        {data?.movies?.map((m) => (
+          <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+        ))}
+      </Movies>
     </Container>
   );
 };
